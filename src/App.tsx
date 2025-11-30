3e80555e0b5574c41b2ee2284f65c85b4a4f3368
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-11-30 12:00:02 +0530

    feat: integrate languageReducer with i18n and import i18n config in main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 import './styles/seettu-3d.css';
 import Router from './routes/Router.tsx';
+import { useSelector } from 'react-redux';
+import type { RootState } from './store/store.ts';
+import i18n from './i18n.ts';
 
 const App: React.FC = () => {
+  const lang = useSelector((s: RootState) => s.language.value);
+
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, [lang]);
+
   return <Router />;
 };
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from '../slices/themeSlice.ts';
 import menuReducer from '../slices/menuSlice.ts';
+import languageReducer from '../slices/languageSlice.ts';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     menu: menuReducer,
+    language: languageReducer,
   },
 });
 
