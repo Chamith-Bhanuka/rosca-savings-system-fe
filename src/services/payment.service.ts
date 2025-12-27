1252aa86ab861b6c880bbfb05d62f7f251679a33
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-12-27 19:33:24 +0530

    feat: create payment.service with getClientSecret and confirmStripePayment functions

---
+++ src/services/payment.service.ts
@@ -0,0 +1,22 @@
+import api from './api.ts';
+
+export const getClientSecret = async (groupId: any, cycle: any) => {
+  const res = await api.post(`/payment/create-intent`, {
+    groupId,
+    cycle,
+  });
+  return res.data;
+};
+
+export const confirmStripePayment = async (
+  groupId: any,
+  cycle: any,
+  paymentId: any
+) => {
+  const res = await api.post(`/payment/confirm`, {
+    groupId,
+    cycle,
+    paymentIntentId: paymentId,
+  });
+  return res.data;
+};
